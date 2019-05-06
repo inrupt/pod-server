8766ffccb39056f2d40c84e808d991a2cb902678
Michiel de Jong <michiel@unhosted.org>
2019-05-06 10:24:16 +0200

    Got /.well-known/ working but not /account/

--- src/server.ts
+++ src/server.ts
@@ -28,22 +28,19 @@ export class Server {
   }
   async listen () {
     this.idpRouter = await defaultConfiguration({
-      issuer: 'https://api.swype.io'
+      issuer: aud,
+      pathPrefix: IDP_PREFIX
     })
     this.app = new Koa()
     debug(this.idpRouter)
-    this.app.use(async (ctx, next) => {
-      if (ctx.req.url && ctx.req.url.substr(IDP_PREFIX.length) === IDP_PREFIX) {
-        return next()
-      } else {
-        debug('LDP handler')
-        this.handler(ctx.req, ctx.res)
-        ctx.respond = false
-      }
-    })
     this.app.use(this.idpRouter.routes())
     this.app.use(this.idpRouter.allowedMethods())
     this.server = this.app.listen(this.port)
+    this.app.use(async (ctx, next) => {
+      debug('LDP handler', ctx.req.method, ctx.req.url)
+      this.handler(ctx.req, ctx.res)
+      ctx.respond = false
+    })
     this.wsServer = new WebSocket.Server({
       server: this.server
     })
@@ -62,7 +59,6 @@ export class Server {
     debug('listening on port', this.port)
   }
   close () {
-    // this.server.close()
     if (this.server) {
       this.server.close()
     }
@@ -73,15 +69,18 @@ export class Server {
   }
 }
 
-// // on startup:
-// const port = parseInt((process.env.PORT ? process.env.PORT : ''), 10) || 8080
+// on startup:
+const port = parseInt((process.env.PORT ? process.env.PORT : ''), 10) || 8080
 
-// const aud = process.env.AUD || 'https://localhost:8443'
-// const server = new Server(port, aud)
-// server.listen()
-// // server.close()
+const aud = process.env.AUD || 'https://localhost:8443'
+const server = new Server(port, aud)
 
-// export function closeServer () {
-//   debug('closing server')
-//   server.close()
-// }
+// tslint:disable-next-line: no-floating-promises
+server.listen()
+
+// server.close()
+
+export function closeServer () {
+  debug('closing server')
+  server.close()
+}
