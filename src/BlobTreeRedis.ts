51b3058e4eee6177640602723a9e64cafdc52ca5
Michiel de Jong <michiel@unhosted.org>
2019-05-17 11:32:42 +0200

    Use a redis transaction for setData

--- src/BlobTreeRedis.ts
+++ src/BlobTreeRedis.ts
@@ -33,11 +33,20 @@ class BlobRedis implements Blob {
     return bufferToStream(Buffer.from(ret))
   }
   async setData (stream: ReadableStream) {
-    await this.checkWatch()
     const value: Buffer = await streamToBuffer(stream)
+    await this.client.multi()
     await this.client.set(this.path.toString(), value.toString())
     const parentPath = this.path.toParent().toString()
     await this.client.hset(parentPath, this.path.toString(), 'yes')
+    // This watch..multi..exec transaction guarantees two things:
+    // 1) the blob wasn't deleted by a different thread inbetween our
+    // `set` call for the blob and our `hset` call for the container.
+    // 2) the contents of the blob didn't change in between any previous
+    // `getData` or `exists` calls on this blob, and our execution of
+    // `setData` here.
+    // See https://redis.io/topics/transactions for more details,
+    // specifically the 'Optimistic locking using check-and-set' section.
+    await this.client.exec()
   }
   async delete () {
     await this.checkWatch()
@@ -84,7 +93,9 @@ export class BlobTreeRedis extends EventEmitter implements BlobTree {
       hkeys: promisify(this.callbacksClient.hkeys).bind(this.callbacksClient),
       hset: promisify(this.callbacksClient.hset).bind(this.callbacksClient),
       quit: promisify(this.callbacksClient.quit).bind(this.callbacksClient),
-      watch: promisify(this.callbacksClient.watch).bind(this.callbacksClient)
+      watch: promisify(this.callbacksClient.watch).bind(this.callbacksClient),
+      multi: promisify(this.callbacksClient.multi).bind(this.callbacksClient),
+      exec: promisify(this.callbacksClient.exec).bind(this.callbacksClient)
     }
   }
   stop () {
