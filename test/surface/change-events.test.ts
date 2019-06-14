c462154bf1b6cfa09635f097cb8ce0b2a5de23be
Michiel de Jong <michiel@unhosted.org>
2019-06-14 13:22:49 +0200

    use MockDate

--- test/surface/change-events.test.ts
+++ test/surface/change-events.test.ts
@@ -2,6 +2,7 @@ import { startServer, stopServer } from './helper'
 import fetch from 'node-fetch'
 import WebSocket from 'ws'
 import fs from 'fs'
+import MockDate from 'mockdate'
 
 let wsClient: WebSocket
 let received: Promise<string>
@@ -9,6 +10,7 @@ let received: Promise<string>
 const bearerToken = fs.readFileSync('test/fixtures/bearerToken')
 
 beforeEach(async () => {
+  MockDate.set(1434319925275)
   startServer(8081)
   const options = await fetch('http://localhost:8081/asdf/test.txt', {
     method: 'HEAD',
@@ -24,17 +26,20 @@ beforeEach(async () => {
   wsClient = new WebSocket(updatesVia)
   received = new Promise((resolve) => {
     wsClient.on('message', function incoming (data) {
+      console.log('subscribed')
       resolve(data.toString())
     })
   })
   await new Promise((resolve) => {
     wsClient.on('open', function open () {
+      console.log('client open, subscribing')
       wsClient.send('sub http://localhost:8081/asdf/')
       resolve(undefined)
     })
   })
 })
 afterEach(() => {
+  MockDate.reset()
   wsClient.close()
   stopServer()
 })
