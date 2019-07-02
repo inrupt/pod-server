99baddec04df079529878b49a995a23b650b58aa
Michiel de Jong <michiel@unhosted.org>
2019-07-02 11:40:57 +0200

    ix server unit test

--- test/unit/server.test.ts
+++ test/unit/server.test.ts
@@ -1,11 +1,12 @@
 import { Server } from '../../src/server'
+import { BlobTreeInMem } from 'wac-ldp'
 
 const PORT = 8082
 
 let server: Server
 
 test('server', async () => {
-  server = new Server({ port: PORT, aud: `http://localhost:${PORT}`, owner: new URL('https://localhost/#me') })
+  server = new Server({ port: PORT, aud: `http://localhost:${PORT}`, owner: new URL('https://localhost/#me'), storage: new BlobTreeInMem() })
   await server.listen()
   server.close()
 })
