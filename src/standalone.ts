626695a4f515b0b70b9d55c2dfa6b56cb7d3b4e5
Michiel de Jong <michiel@unhosted.org>
2019-07-02 11:54:10 +0200

    fix https://github.com/inrupt/wac-ldp/issues/56

--- src/standalone.ts
+++ src/standalone.ts
@@ -1,11 +1,15 @@
 import { Server } from './server'
 import { BlobTreeRedis } from './BlobTreeRedis'
 import * as fs from 'fs'
+import { BlobTreeInMem } from 'wac-ldp'
+import Debug from 'debug'
+
+const debug = Debug('standalone')
 
 // on startup:
-const port = parseInt((process.env.PORT ? process.env.PORT : ''), 10) || 3000
+const port = parseInt((process.env.PORT ? process.env.PORT : ''), 10) || 8080
 
-const aud = process.env.AUD || 'http://localhost:3000'
+const aud = process.env.AUD || `http://localhost:${port}`
 
 const skipWac: boolean = !!process.env.SKIP_WAC
 
@@ -24,12 +28,20 @@ if (!ownerStr) {
   ownerStr = 'https://jackson.solid.community/profile/card#me'
 }
 
+let storage
+if (process.env.REDIS_URL) {
+  debug('using redis backend')
+  storage =  new BlobTreeRedis(process.env.REDIS_URL)
+ } else {
+  debug('using in-memory backend')
+  storage = new BlobTreeInMem()
+ } 
 const server = new Server({
   port,
   aud,
   httpsConfig,
   owner: new URL(ownerStr),
-  storage: new BlobTreeRedis(process.env.REDIS_URL) // Redis-based BlobTree storage
+  storage
 })
 
 async function startServer () {
