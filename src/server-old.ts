55a7d53d74d164a47962e1363924e5576a5f6ce1
jaxoncreed <jaxoncreed@egmail.com>
2019-08-26 22:02:44 -0400

    Started defining blue sky config

---
+++ src/server-old.ts
@@ -0,0 +1,168 @@
+import * as http from 'http'
+import * as https from 'https'
+import * as fs from 'fs'
+import Debug from 'debug'
+import path from 'path'
+import { BlobTree, WacLdp, WacLdpOptions, QuadAndBlobStore } from 'wac-ldp'
+import * as WebSocket from 'ws'
+import { Hub } from 'websockets-pubsub'
+import Koa from 'koa'
+import session from 'koa-session'
+import Router from 'koa-router'
+import { provisionProfile, provisionStorage } from './provision'
+import { defaultConfiguration } from 'solid-idp'
+import getRootRenderRouter from './rootRender'
+// import { default as Accepts } from 'accepts'
+
+const debug = Debug('server')
+
+const LOGIN_HTML = fs.readFileSync('./static/popup.html')
+const DATA_BROWSER_HTML = fs.readFileSync('./static/index.html')
+
+interface HttpsConfig {
+  key: Buffer
+  cert: Buffer
+}
+
+interface OptionsObject {
+  port: number
+  rootDomain: string
+  httpsConfig?: HttpsConfig
+  storage: BlobTree
+  keystore: any,
+  useHttps: boolean
+}
+
+export class Server {
+  storage: BlobTree
+  server: http.Server | undefined
+  hub: Hub | undefined
+  port: number
+  wsServer: any
+  app: Koa | undefined
+  // idpRouter: any
+  rootDomain: string
+  rootOrigin: string
+  wacLdp: WacLdp
+  httpsConfig: HttpsConfig | undefined
+  useHttps: boolean
+  keystore: any
+  constructor (options: OptionsObject) {
+    this.port = options.port
+    this.rootDomain = options.rootDomain
+    this.httpsConfig = options.httpsConfig
+    this.useHttps = options.useHttps
+    this.keystore = options.keystore
+    this.rootOrigin = `http${(this.useHttps ? 's' : '')}://${this.rootDomain}`
+    this.storage = options.storage
+    this.wacLdp = new WacLdp({
+      storage: new QuadAndBlobStore(this.storage),
+      aud: this.rootOrigin,
+      updatesViaUrl: this.webSocketUrl(),
+      skipWac: false,
+      // idpHost: options.rootDomain,
+      usesHttps: true
+    } as WacLdpOptions)
+  }
+  webSocketUrl () {
+    return new URL(`ws${(this.useHttps ? 's' : '')}://${this.rootDomain}`)
+  }
+  storageRootStrToWebIdStr (storageRoot: string) {
+    return storageRoot + (storageRoot.substr(-1) === '/' ? '' : '/') + 'profile/card#me'
+  }
+  screenNameToStorageRootStr (screenName: string) {
+    // no need to append portSuffix here since it's already part of this.rootDomain
+    // const defaultPort: Number = (this.useHttps ? 443 : 80)
+    // const portIsDefault: boolean = (this.port === defaultPort)
+    // const portSuffix: string = (portIsDefault ? '' : `:${this.port}`)
+    // return `http${(this.useHttps ? 's' : '')}://${screenName}.${this.rootDomain}${portSuffix}`
+    return `http${(this.useHttps ? 's' : '')}://${screenName}.${this.rootDomain}`
+  }
+  async listen () {
+    debug('setting IDP issuer to', this.rootDomain)
+    // this.idpRouter = await defaultConfiguration({
+    //   issuer: this.rootOrigin,
+    //   pathPrefix: '',
+    //   webIdFromUsername: async screenname => this.storageRootStrToWebIdStr(this.screenNameToStorageRootStr(screenname)),
+    //   keystore: this.keystore
+    // })
+    this.app = new Koa()
+    this.app.proxy = true
+    this.app.keys = [ 'REPLACE_THIS_LATER' ]
+    this.app.use(session(this.app))
+    this.app.use(async (ctx, next) => {
+      ctx.req.headers['x-forwarded-proto'] = `http${this.useHttps ? 's' : ''}`
+      await next()
+    })
+
+    // this.app.use(this.idpRouter.routes())
+    // this.app.use(this.idpRouter.allowedMethods())
+
+    const rootRenderRouter = getRootRenderRouter(this.rootOrigin)
+    this.app.use(rootRenderRouter.routes())
+    this.app.use(rootRenderRouter.allowedMethods())
+
+    // HACK: in order for the login page to show up, a separate file must be run at /.well-known/solid/login which I find very dirty -- jackson
+    const loginRouter = new Router()
+    loginRouter.get('/.well-known/solid/login', (ctx, next) => {
+      debug('sending login html')
+      ctx.res.writeHead(200, {})
+      ctx.res.end(LOGIN_HTML)
+      ctx.respond = false
+    })
+    this.app.use(loginRouter.routes())
+    this.app.use(loginRouter.allowedMethods())
+
+    // Data Browser
+    this.app.use(async (ctx, next) => {
+      if (ctx.accepts(['text/turtle', 'application/ld+json', 'html']) === 'html') {
+        debug('redirect to data browser!')
+        ctx.res.writeHead(200, {})
+        ctx.res.end(DATA_BROWSER_HTML)
+      } else {
+        debug('skipping data browser')
+        await next()
+      }
+    })
+    // END HACK
+
+    this.app.use(async (ctx, next) => {
+      // debug(ctx.req.headers, ctx.req.headers['accept'] && ctx.req.headers['accept'].indexOf('text/html'))
+      // if ((ctx.req.headers['accept']) && (ctx.req.headers['accept'].indexOf('text/html') !== -1)) {
+      //   ctx.res.writeHead(200, {})
+      //   ctx.res.end(DATA_BROWSER_HTML)
+      //   ctx.respond = false
+      // } else {
+      debug('LDP handler', ctx.req.method, ctx.req.url)
+      await this.wacLdp.handler(ctx.req, ctx.res)
+      ctx.respond = false
+      // }
+    })
+    if (this.httpsConfig) {
+      this.server = https.createServer(this.httpsConfig, this.app.callback())
+    } else {
+      this.server = http.createServer(this.app.callback())
+    }
+    this.server.listen(this.port)
+    this.wsServer = new WebSocket.Server({
+      server: this.server
+    })
+    this.hub = new Hub(this.wacLdp, this.rootOrigin)
+    this.wsServer.on('connection', this.hub.handleConnection.bind(this.hub))
+    this.wacLdp.on('change', (event: { url: URL }) => {
+      if (this.hub) {
+        this.hub.publishChange(event.url)
+      }
+    })
+    debug('listening on port', this.port, (this.httpsConfig ? 'https' : 'http'))
+  }
+  close () {
+    if (this.server) {
+      this.server.close()
+    }
+    if (this.wsServer) {
+      this.wsServer.close()
+    }
+    debug('closing port', this.port)
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,168 +1,234 @@
-import * as http from 'http'
-import * as https from 'https'
-import * as fs from 'fs'
-import Debug from 'debug'
-import path from 'path'
-import { BlobTree, WacLdp, WacLdpOptions, QuadAndBlobStore } from 'wac-ldp'
-import * as WebSocket from 'ws'
-import { Hub } from 'websockets-pubsub'
-import Koa from 'koa'
-import session from 'koa-session'
-import Router from 'koa-router'
-import { provisionProfile, provisionStorage } from './provision'
-import { defaultConfiguration } from 'solid-idp'
-import getRootRenderRouter from './rootRender'
-// import { default as Accepts } from 'accepts'
+import { Context, Middleware } from "koa";
+import { exists } from "fs";
+import Router from "koa-router";
+import { WacLdp } from "wac-ldp";
+import { networkInterfaces } from "os";
+import { maxHeaderSize } from "http";
 
-const debug = Debug('server')
-
-const LOGIN_HTML = fs.readFileSync('./static/popup.html')
-const DATA_BROWSER_HTML = fs.readFileSync('./static/index.html')
-
-interface HttpsConfig {
-  key: Buffer
-  cert: Buffer
+{
+  "storage": {
+    "type": "filesystem",
+    "rootFolder": "/home/jackson/pod-server/data"
+  },
+  "network": {
+    "host": "api.swype.io",
+    "port": 3000,
+    "useHttps": true
+  },
+  "htmlRenderer": "mashlib",
+  "allowProvisioningExternalIdentites": false,
+  "routing": {
+    "routingStrategy": "subdomain",
+    "pathPrefix": ""
+  },
+  "allowExternalDomains": false,
+  "identityProvider": {
+    "enabled": true,
+    "storage": {
+      "type": "filesystem",
+      "rootFolder": "/home/jackson/pod-server/.db"
+    },
+    "keystore": "/home/jackson/pod-server/sample-keystore.json",
+    "email": {
+      "host": "smtp.sendgrid.net",
+      "port": "465",
+      "secure": true,
+      "auth": {
+        "user": "inrupt",
+        "pass": "V@ss@rStr33t"
+      }
+    }
+  }
 }
 
-interface OptionsObject {
-  port: number
-  rootDomain: string
-  httpsConfig?: HttpsConfig
-  storage: BlobTree
-  keystore: any,
-  useHttps: boolean
+
+
+
+
+
+export interface IDPConfiguration {
+
 }
 
-export class Server {
-  storage: BlobTree
-  server: http.Server | undefined
-  hub: Hub | undefined
-  port: number
-  wsServer: any
-  app: Koa | undefined
-  // idpRouter: any
-  rootDomain: string
-  rootOrigin: string
-  wacLdp: WacLdp
-  httpsConfig: HttpsConfig | undefined
-  useHttps: boolean
-  keystore: any
-  constructor (options: OptionsObject) {
-    this.port = options.port
-    this.rootDomain = options.rootDomain
-    this.httpsConfig = options.httpsConfig
-    this.useHttps = options.useHttps
-    this.keystore = options.keystore
-    this.rootOrigin = `http${(this.useHttps ? 's' : '')}://${this.rootDomain}`
-    this.storage = options.storage
-    this.wacLdp = new WacLdp({
-      storage: new QuadAndBlobStore(this.storage),
-      aud: this.rootOrigin,
-      updatesViaUrl: this.webSocketUrl(),
-      skipWac: false,
-      // idpHost: options.rootDomain,
-      usesHttps: true
-    } as WacLdpOptions)
+
+// Should the value be "any" or "string / buffer" ie should the storage layer know what it's processing
+export interface PodStorage {
+  get(prefix: string, key: string): Promise<any>
+  set(prefix: string, key: string, value: any): Promise<void>
+  delete(prefix: string, key: string): Promise<void>
+}
+
+// Is there a need for this to be a separate interface?
+export interface ResourceApi {
+  rootUrl: string
+  exists(path: string): Promise<boolean>
+  get(path: string): Promise<string>
+  set(path: string, value: string | Buffer): Promise<void>
+  delete(path: string): Promise<void>
+}
+
+// Should this be a koa response object?
+interface rawLDPResponse {
+  body: string | Buffer,
+  headers: {
+    [key: string]: string
   }
-  webSocketUrl () {
-    return new URL(`ws${(this.useHttps ? 's' : '')}://${this.rootDomain}`)
-  }
-  storageRootStrToWebIdStr (storageRoot: string) {
-    return storageRoot + (storageRoot.substr(-1) === '/' ? '' : '/') + 'profile/card#me'
-  }
-  screenNameToStorageRootStr (screenName: string) {
-    // no need to append portSuffix here since it's already part of this.rootDomain
-    // const defaultPort: Number = (this.useHttps ? 443 : 80)
-    // const portIsDefault: boolean = (this.port === defaultPort)
-    // const portSuffix: string = (portIsDefault ? '' : `:${this.port}`)
-    // return `http${(this.useHttps ? 's' : '')}://${screenName}.${this.rootDomain}${portSuffix}`
-    return `http${(this.useHttps ? 's' : '')}://${screenName}.${this.rootDomain}`
-  }
-  async listen () {
-    debug('setting IDP issuer to', this.rootDomain)
-    // this.idpRouter = await defaultConfiguration({
-    //   issuer: this.rootOrigin,
-    //   pathPrefix: '',
-    //   webIdFromUsername: async screenname => this.storageRootStrToWebIdStr(this.screenNameToStorageRootStr(screenname)),
-    //   keystore: this.keystore
-    // })
-    this.app = new Koa()
-    this.app.proxy = true
-    this.app.keys = [ 'REPLACE_THIS_LATER' ]
-    this.app.use(session(this.app))
-    this.app.use(async (ctx, next) => {
-      ctx.req.headers['x-forwarded-proto'] = `http${this.useHttps ? 's' : ''}`
-      await next()
-    })
+  status: number
+}
 
-    // this.app.use(this.idpRouter.routes())
-    // this.app.use(this.idpRouter.allowedMethods())
+/**
+ * ctx: the context object for the request
+ * rawResponse: The raw response from the LDP before rendering and sending
+ */
+type contentRenderer = (ctx: Context, rawResponse: rawLDPResponse) => Promise<void>
 
-    const rootRenderRouter = getRootRenderRouter(this.rootOrigin)
-    this.app.use(rootRenderRouter.routes())
-    this.app.use(rootRenderRouter.allowedMethods())
+export interface ProvisioningConfiguration {
+  allowExternalProvisioning: boolean
+  provisionRoute?: routeQualifier
 
-    // HACK: in order for the login page to show up, a separate file must be run at /.well-known/solid/login which I find very dirty -- jackson
-    const loginRouter = new Router()
-    loginRouter.get('/.well-known/solid/login', (ctx, next) => {
-      debug('sending login html')
-      ctx.res.writeHead(200, {})
-      ctx.res.end(LOGIN_HTML)
-      ctx.respond = false
-    })
-    this.app.use(loginRouter.routes())
-    this.app.use(loginRouter.allowedMethods())
+}
 
-    // Data Browser
-    this.app.use(async (ctx, next) => {
-      if (ctx.accepts(['text/turtle', 'application/ld+json', 'html']) === 'html') {
-        debug('redirect to data browser!')
-        ctx.res.writeHead(200, {})
-        ctx.res.end(DATA_BROWSER_HTML)
-      } else {
-        debug('skipping data browser')
-        await next()
-      }
-    })
-    // END HACK
+type routerFactory = (handler: (ctx: Context, next: Function) => Promise<void>) => Promise<Router>
 
-    this.app.use(async (ctx, next) => {
-      // debug(ctx.req.headers, ctx.req.headers['accept'] && ctx.req.headers['accept'].indexOf('text/html'))
-      // if ((ctx.req.headers['accept']) && (ctx.req.headers['accept'].indexOf('text/html') !== -1)) {
-      //   ctx.res.writeHead(200, {})
-      //   ctx.res.end(DATA_BROWSER_HTML)
-      //   ctx.respond = false
-      // } else {
-      debug('LDP handler', ctx.req.method, ctx.req.url)
-      await this.wacLdp.handler(ctx.req, ctx.res)
-      ctx.respond = false
-      // }
-    })
-    if (this.httpsConfig) {
-      this.server = https.createServer(this.httpsConfig, this.app.callback())
-    } else {
-      this.server = http.createServer(this.app.callback())
+/**
+ * The Pod configuration is for internal use within the PodServer repo
+ * Essentially the PodOptions (listed later) will translate into a Pod Configuration
+ */
+export interface PodConfiguration {
+  storage: PodStorage
+  network: {
+    port: number
+    ssl?: {
+      key: string
+      cert: string
     }
-    this.server.listen(this.port)
-    this.wsServer = new WebSocket.Server({
-      server: this.server
-    })
-    this.hub = new Hub(this.wacLdp, this.rootOrigin)
-    this.wsServer.on('connection', this.hub.handleConnection.bind(this.hub))
-    this.wacLdp.on('change', (event: { url: URL }) => {
-      if (this.hub) {
-        this.hub.publishChange(event.url)
-      }
-    })
-    debug('listening on port', this.port, (this.httpsConfig ? 'https' : 'http'))
+  },
+  // The developer sets up a very simple router that just calls a handler on the right route
+  // The handler will manage calling WAC LDP and the content renderer after that
+  ldpRouterFactory: routerFactory 
+  contentRenderers: { // Could be html, ttl, json, json+ld, any possible content type, and/or default (for the catch all renderer)
+    [contentType: string]: contentRenderer
+  },
+  // The root url of the admin pod upon startup, if this does not exist, it will automatically be provisioned
+  adminPodRoot: string
+  provisioningConfiguration: {
+
+    canCreate(rootUrl: string, resourceApi: ResourceApi): Promise<void>
+    provision(rootUrl: string, resourceApi: ResourceApi): Promise<void>
+    delete(rootUrl: string, resourceApi: ResourceApi): Promise<void>
   }
-  close () {
-    if (this.server) {
-      this.server.close()
+}
+
+/**
+ * Pod Options defines the full range of options that can be passed into the server
+ * This is how the developer interacts with the server object
+ */
+export interface PodOptions {
+  // How this pod will store data
+  storage: {
+    // Choose between built in types and custom
+    type: 'redis' | 'filesystem' | 'custom'
+    // If custom is selected the develoepr can insert code
+    customPodStorage?: PodStorage
+    // If redis is selected the url of the box
+    redisUrl?: string,
+    // If filesystem is selected, the root folder for the filesystem
+    rootFolder?: string
+  },
+  // Configuration for how the server will broadcast itself
+  network: {
+    // The port on which it will listen
+    port: number,
+    // ssl config if the server should handle its own https (instead of a proxy)
+    ssl?: {
+      // True if self hosting ssl is enabled
+      enabled?: boolean
+      // Path to the pem key
+      key: string
+      // Path to the pem cert
+      cert: string
     }
-    if (this.wsServer) {
-      this.wsServer.close()
+  },
+  // Routing strategy for that will resolve to the ldp
+  ldpRouting: {
+    // Select between different strategies "subdomain" to only route to the ldp on direct subdomains, "path" to distinguish between paths
+    type: 'subdomain' | 'path' | 'custom',
+    // If custom is selected the developer can insert code
+    customRouterFactory?: routerFactory
+    // The protocol the ldp should broadcast it is using
+    protocol: 'http' | 'https'
+    // An optional prefix to insert before the Pod roots in the url path (example "/pods")
+    pathPrefix?: string
+    // The host of the server
+    rootHost?: string
+    // If true, the router will pass any requests from origins that do not match its own to the ldp
+    allowExternalOrigins?: boolean
+  },
+  // Define how data is rendered for different content types
+  contentRenderers: {
+    // The content type (example "html", "ttl"...)
+    [contentType: string]: {
+      // Select between default renderers or choose custom. Mashlib will return mashlib
+      type: 'mashlib' | 'raw' | 'custom'
+      // If custom is selected the developer can insert code
+      customHtmlRenderer?: contentRenderer
     }
-    debug('closing port', this.port)
   }
+  // Configuration for the location of the Admin pod. This pod will contain information important to the pod provider (like usage statistics) as well as information that users can access (lke quotas and purchase history)
+  adminPod: {
+    // The type of the admin pod. Local if it should be hosted on this pod or external if it should be elsewhere
+    type: 'local' | 'external',
+    // The webID of the admin Pod
+    webID: string,
+    // The root url of the admin pod
+    podRoot: string,
+    // For external pods. A client secret to log in with the client credentials flow
+    clientSecret: string
+  }
+  // Configuration for limiting automatically managing Pods (These are less fully thought out)
+  podManagement: {
+    // Thype of management
+    type: 'none' | 'hard-quota' | 'paid-tiers' | 'custom'
+    quota: number
+    paymentMethod: PaymentMethod
+    email: EmailConfig
+    tiers: ({
+      minSize: number
+      maxHeaderSize: number
+      cost: number
+    })[]
+  }
+  // Configuration to allow users with external identity providers to provision pods
+  externalProvisioning?: {
+    // Is external provisioning allowed
+    enabled?: boolean
+    // Routing strategy that will resolve to the external provisioning
+    externalProvisioningRouting: {
+      // Default or custom routing
+      type: 'default' | 'custom'
+      // If custom the developer can insert code here
+      customRouterFactory?: routerFactory
+      // An optional prefix to insert before each route
+      pathPrefix: string
+    }
+    uiRenderers: {
+      getProvision: Middleware
+      postProvision: Middleware
+    }
+  }
+  // 
+  externalDomains?: {
+    enabled?: boolean
+    externalDomainRouting: {
+      type: 'default' | 'custom'
+      customRouterFactory?: routerFactory
+      pathPrefix: string
+    }
+    uiRenderers: {
+      getDomain: Middleware
+      postDomain: Middleware
+    }
+    requireProofOfExternalDomain?: boolean
+  }
+
 }
