241b859db61e7153c2a38e606f5757fade570339
Michiel de Jong <michiel@unhosted.org>
2019-09-20 12:39:26 +0200

    tests passing

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,8 @@ module.exports = {
         'ts-jest': {
             // reference: https://kulshekhar.github.io/ts-jest/user/config/
         }
-    }
+    },
+    transformIgnorePatterns: [
+        'node_modules/(?!(solid-server-ts|.*/node_modules/solid-server-ts)/)'
+    ]
 };
